9ab061621800d43cae0f88e3ad6c85d0bf493d7b
Chris <cgoetting@rcsmaine.com>
2026-08-23 06:12:50 +0000

    feat: prefix matching for FTS5 book search

--- internal/handlers/pages/books_test.go
+++ internal/handlers/pages/books_test.go
@@ -8,16 +8,16 @@ func TestSafeFTS5Query(t *testing.T) {
 		in   string
 		want string
 	}{
-		{"apostrophe", "We're going", `"We're" "going"`},
-		{"contraction", "don't", `"don't"`},
-		{"embedded double quote", `it's "quoted"`, `"it's" """quoted"""`},
-		{"dash operator", "a - b", `"a" "-" "b"`},
-		{"column filter", "col1:col2", `"col1:col2"`},
-		{"prefix star", "*", `"*"`},
-		{"unclosed paren", "(unclosed", `"(unclosed"`},
-		{"AND keyword", "a AND b", `"a" "AND" "b"`},
-		{"NEAR operator", "NEAR(a b)", `"NEAR(a" "b)"`},
-		{"extra whitespace", "  hello    world  ", `"hello" "world"`},
+		{"apostrophe", "We're going", `"We're"* "going"*`},
+		{"contraction", "don't", `"don't"*`},
+		{"embedded double quote", `it's "quoted"`, `"it's"* """quoted"""*`},
+		{"dash operator", "a - b", `"a"* "-"* "b"*`},
+		{"column filter", "col1:col2", `"col1:col2"*`},
+		{"prefix star", "*", `"*"*`},
+		{"unclosed paren", "(unclosed", `"(unclosed"*`},
+		{"AND keyword", "a AND b", `"a"* "AND"* "b"*`},
+		{"NEAR operator", "NEAR(a b)", `"NEAR(a"* "b)"*`},
+		{"extra whitespace", "  hello    world  ", `"hello"* "world"*`},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
